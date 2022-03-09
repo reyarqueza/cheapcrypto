9060268716f53e50f3562bc869b47a270fc650cc
Rey Arqueza <reyarqueza@gmail.com>
2022-03-09 08:26:45 -0800

    Listing component

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
+import Listing from './Listing.jsx';
 
 export default function (props) {
   return (
     <div>
       <h1>Cheap Crypto</h1>
-      <table border="1">
-        <tr>
-          <th>Name</th>
-          <th>Symbol</th>
-          <th>Platform</th>
-          <th>Quote</th>
-        </tr>
-        {props.listings.map(prop => {
-          return (
-            <tr key={prop.id}>
-              <td>{prop.name}</td>
-              <td>{prop.symbol}</td>
-              <td>{prop.platform.symbol}</td>
-              <td>{prop.quote.USD.price}</td>
-            </tr>
-          );
-        })}
-      </table>
+      <Listing {...props} />
     </div>
   );
 }
