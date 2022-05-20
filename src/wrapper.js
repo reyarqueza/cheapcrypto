94c71d6df55798f7707b84bfba064922c61501ce
Rey Arqueza <reyarqueza@gmail.com>
2022-05-20 13:42:49 -0700

    google analytics

--- src/wrapper.js
+++ src/wrapper.js
@@ -20,6 +20,17 @@ export default function (content, preloadedState) {
     window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
     </script>
     <script defer src="/js/bundle.js?v=0.1.13"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-RJ2DCZT5YN"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-RJ2DCZT5YN');
+    </script>
+    
   </head>
   <body>
     <script src="https://accounts.google.com/gsi/client" async defer></script>
